54a4cea1fa9a92433db58ef14c1222359fa2137c
Greg Bowler <greg.bowler@g105b.com>
2018-09-04 16:31:02 +0100

    Add menu key

--- keyboards/greg/keymaps/iso-greg/keymap.c
+++ keyboards/greg/keymaps/iso-greg/keymap.c
@@ -9,19 +9,6 @@
 #define MD_PAUS KC_MEDIA_PLAY_PAUSE
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  /* Keymap _BL: (Base Layer) Default Layer
-   * ,----------------------------------------------------------------.
-   * |Esc | 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |~ ` |
-   * |----------------------------------------------------------------|
-   * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|     |Del |
-   * |-------------------------------------------------------    -----|
-   * |CAPS   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;| '|  #|Entr|Home|
-   * |----------------------------------------------------------------|
-   * |Shift|  \ |  Z|  X|  C|  V|  B|  N|  M|  ,|  .| /|Rshift|Up|End |
-   * |----------------------------------------------------------------|
-   * |Ctrl|Win |Alt |        Space          |Alt| FN|Ctrl|Lef|Dow|Rig |
-   * `----------------------------------------------------------------'
-   */
   [_BL] = LAYOUT_iso(
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_GRV,  \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_NUHS, KC_DEL,  \
@@ -29,25 +16,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_END, \
     KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                             KC_RALT, MO(_FL), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
 	),
-
-  /* Keymap _FL1: Function Layer 1
-   * ,----------------------------------------------------------------.
-   * |   | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12| Delete|    |
-   * |----------------------------------------------------------------|
-   * |     |   | ↑ |   |   |   |   |   |   |   |   |   |   |     |Ins |
-   * |-------------------------------------------------------    -----|
-   * |       | ← | ↓ | → |   |   |   |   |   |   |   |  |   |    |PgUp|
-   * |----------------------------------------------------------------|
-   * |     |   |   |   | L+|LED| L-|   | V+|  V-|Mut|  | MsBtn|Up|PgDn|
-   * |----------------------------------------------------------------|
-   * |    |    |    |                       |   |   |    | Lt| Dn| Rt |
-   * `----------------------------------------------------------------'
-   */
   [_FL] = LAYOUT_iso(
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_INS,  \
     _______, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, MD_PAUS, MD_PREV, MD_NEXT, _______, KC_INS, \
     _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______, _______,          _______, KC_PGUP,  \
     _______, BL_TOGG, BL_DEC,  BL_INC , _______, _______, _______,  _______, _______, KC_VOLD, KC_VOLU, KC_MUTE, KC_BTN1, KC_MS_U, KC_PGDN, \
-    _______, _______, _______,                   _______,                            _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_R
+    _______, _______, _______,                   _______,                            KC_APP  , _______, _______, KC_MS_L, KC_MS_D, KC_MS_R
 	),
 };
